f953b57fdbc0044686f6bc1bdaede3321b929151
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-28 00:00:50 -0500

    dfd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 # ecs.tf
-
+# Definición del cluster ECS
 resource "aws_ecs_cluster" "my_ecs_cluster_jdhg" {
     name = "my_ecs_cluster_jdhg"
 }
 
+# Plantilla para la definición de la tarea ECS
 data "template_file" "cb_app" {
-    template = file("./ecs/task_definition.tpl") 
+    template = file("./ecs/task_definition.tpl")
 
     vars = {
         app_image      = var.app_image
@@ -16,6 +17,7 @@ data "template_file" "cb_app" {
     }
 }
 
+# Definición de la tarea ECS
 resource "aws_ecs_task_definition" "app" {
     family                   = "cb-app-task"
     execution_role_arn       = aws_iam_role.ecs_exec_role_jdhg.arn
@@ -26,24 +28,28 @@ resource "aws_ecs_task_definition" "app" {
     container_definitions    = data.template_file.cb_app.rendered
 }
 
+# Definición del servicio ECS
 resource "aws_ecs_service" "main" {
     name            = "cb-service"
-    cluster         = aws_ecs_cluster.my_ecs_cluster_jdhg.id
+    cluster         = aws_ecs_cluster.my_ecs_cluster_jdhg.id  # Corregido: Utilizar el ID del cluster
     task_definition = aws_ecs_task_definition.app.arn
     desired_count   = var.app_count
     launch_type     = "FARGATE"
 
+    # Configuración de red
     network_configuration {
         security_groups  = [aws_security_group.ecs_tasks-jdhg.id]
         subnets          = aws_subnet.private-jdhg-west-b.*.id
         assign_public_ip = true
     }
 
+    # Configuración del balanceador de carga (ALB)
     load_balancer {
-        target_group_arn = aws_alb_target_group.apppy.id
+        target_group_arn = aws_alb_target_group.apppy.arn  # Asegurarse que el ARN del target group sea correcto
         container_name   = "cb-app"
         container_port   = var.app_port
     }
 
+    # Dependencias
     depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_exec_role_policy]
 }
