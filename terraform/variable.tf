17ea2273b7f62b350d9facd1a11f98dae1291386
Jafed Hernandez <jafedhernandez@482065.local>
2024-06-28 06:52:16 -0500

    dfd

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -20,7 +20,7 @@ variable "az_count" {
 
 variable "app_image" { 
     description = "Imagen de Docker para ejecutar en el clúster ECS" 
-    default = "python:3.8-slim-buster"
+    default = "${secrets.AWS_ACCOUNT_ID }.dkr.ecr.us-west-2.amazonaws.com/cb-app"
 } 
 
 variable "app_port" { 
